a394629f357f73350b686708596be405763e16ee
rtolesnikov <rtolesnikov@yahoo.com>
2023-11-05 15:05:09 -0800

    Process 2023 VA and 2023 UR10 from archive. Adjust github action to only include new files that are output from the processing instead of all files.

---
+++ src/activate_astro.cmd
@@ -0,0 +1,4 @@
+REM %windir%\System32\cmd.exe "/K" C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
+%windir%\System32\cmd.exe "/K" C:\ProgramData\Anaconda3\Scripts\activate.bat C:\Users\rtole\.conda\envs\astro
+REM Activate Powershell
+REM %windir%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& 'C:\ProgramData\Anaconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\ProgramData\Anaconda3' "
